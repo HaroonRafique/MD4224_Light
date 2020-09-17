08a958f2205b53987276fb7ad16466cd1d90a1bc
Haroon Rafique <haroon.rafique@protonmail.com>
2020-09-18 00:54:05 +0200

    08: Injection Bump: Updated for testing

--- 08_Injection_Bump/00_Master/PyORBIT/simulation_parameters.py
+++ 08_Injection_Bump/00_Master/PyORBIT/simulation_parameters.py
@@ -99,18 +99,18 @@ switches = {
 if space_charge_flag:
         switches['Space_Charge'] = True        
         if transverse_plane == 'H': 
-                parameters['input_distn'] = '../../../01_Generate_Initial_Distribution/' + str(parameters['n_macroparticles']) + '/1_H_21/bunch_output/mainbunch_-000001.mat'
+                parameters['input_distn'] = '../../../01_Generate_Initial_Distribution/500000/1_H_21/bunch_output/mainbunch_-000001.mat'
                 # ~ parameters['input_distn'] = '../../../07_Dispersion_Mismatch/Initial_Distributions/' + str(parameters['n_macroparticles']) + '/1_H_21/bunch_output/mainbunch_-000001.mat'
         else:
-                parameters['input_distn'] = '../../../01_Generate_Initial_Distribution/' + str(parameters['n_macroparticles']) + '/1_V_24/bunch_output/mainbunch_-000001.mat'
+                parameters['input_distn'] = '../../../01_Generate_Initial_Distribution/500000/1_V_24/bunch_output/mainbunch_-000001.mat'
                 # ~ parameters['input_distn'] = '../../../07_Dispersion_Mismatch/Initial_Distributions/' + str(parameters['n_macroparticles']) + '/1_V_24/bunch_output/mainbunch_-000001.mat'
 else:
         switches['Space_Charge'] = False
         if transverse_plane == 'H': 
-                parameters['input_distn'] = '../../../01_Generate_Initial_Distribution/' + str(parameters['n_macroparticles']) + '/0_H_21/bunch_output/mainbunch_-000001.mat'
+                parameters['input_distn'] = '../../../01_Generate_Initial_Distribution/500000/0_H_21/bunch_output/mainbunch_-000001.mat'
                 # ~ parameters['input_distn'] = '../../../07_Dispersion_Mismatch/Initial_Distributions/' + str(parameters['n_macroparticles']) + '/0_H_21/bunch_output/mainbunch_-000001.mat'
         else:
-                parameters['input_distn'] = '../../../01_Generate_Initial_Distribution/' + str(parameters['n_macroparticles']) + '/0_V_24/bunch_output/mainbunch_-000001.mat'
+                parameters['input_distn'] = '../../../01_Generate_Initial_Distribution/500000/0_V_24/bunch_output/mainbunch_-000001.mat'
                 # ~ parameters['input_distn'] = '../../../07_Dispersion_Mismatch/Initial_Distributions/' + str(parameters['n_macroparticles']) + '/0_V_24/bunch_output/mainbunch_-000001.mat'
 
 # PTC RF Table Parameters
